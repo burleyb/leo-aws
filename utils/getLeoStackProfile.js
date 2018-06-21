77be4256c6723c377001f56fe8d1b8d749298901
Steve <steve@roaringdevelopment.com>
2018-06-20 21:26:15 -0600

    changed format

--- utils/getLeoStackProfile.js
+++ utils/getLeoStackProfile.js
@@ -1,26 +1,16 @@
 'use strict';
 
-module.exports = async function (stackname, configuration) {
+module.exports = async function(stackname, configuration) {
 	const leoaws = require('../index')(configuration);
 
 	let resources = await leoaws.cloudformation.getStackResources(stackname);
 
 	let profile = {
-		region: configuration.region,
-		resources: {
-			"Region": configuration.region
-		}
+		"Region": configuration.region
 	};
 	Object.keys(resources).forEach((id) => {
-		if (id == "LeoKinesisStream") {
-			profile.kinesis = resources[id].id;
-		} else if (id == "LeoFirehoseStream") {
-			profile.firehose = resources[id].id;
-		} else if (id == "LeoS3") {
-			profile.s3 = resources[id].id;
-		}
 		if (resources[id].type.match(/Table|Bucket|DeliveryStream|Stream/)) {
-			profile.resources[id] = resources[id].id;
+			profile[id] = resources[id].id;
 		}
 	});
 
