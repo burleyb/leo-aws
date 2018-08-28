2d86fdc100a885c3372fc56ee0faa4d0d9140fd6
Francis Lewis <flewis@leoinsights.com>
2018-08-28 13:25:57 -0600

    leo-aws@1.4.0 (#6)
    * Added factory to lazy-load any AWS service into an object which includes _service, or any AWS service that we've built wrappers around.

---
+++ factory.js
@@ -0,0 +1,30 @@
+const config = require("leo-config");
+
+const leoaws = {
+	cloudformation: require('./lib/cloudformation'),
+	dynamodb: require("./lib/dynamodb"),
+	kms: require("./lib/kms"),
+	secrets: require("./lib/secretsmanager"),
+	sqs: require("./lib/sqs")
+};
+const AWS = require('aws-sdk');
+
+module.exports = function (service) {
+	const configuration = config.leoaws;
+	if (configuration && configuration.profile && !configuration.credentials) {
+		configuration.credentials = new AWS.SharedIniFileCredentials({
+			profile: configuration.profile,
+			role: configuration.role
+		});
+	}
+
+	const lowerService = service.toLowerCase();
+	if (leoaws[lowerService]) {
+		return leoaws[lowerService](configuration);
+	} else {
+		// return a configured AWS service
+		return {
+			_service: new AWS[service](configuration)
+		};
+	}
+};
